8c69add8b172dc19f3d4f1d72bb6ab7e23de0529
SSCharan-99 <saisricharan99@msitprogram.net>
2021-05-26 17:14:13 +0530

    make it better

--- start_services.sh
+++ start_services.sh
@@ -2,3 +2,4 @@ cd app/rasa/
 # Start rasa server with nlu model
 rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug \
          -p $PORT
+rasa run actions
