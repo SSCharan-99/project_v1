548a10b141775bace8cc8231e30231ffb7959181
SSCharan-99 <saisricharan99@msitprogram.net>
2021-05-26 18:53:25 +0530

    check commit

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
+npm start
 cd app/rasa/
 # Start rasa server with nlu model
 rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug \
