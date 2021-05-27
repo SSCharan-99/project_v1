efd28759400fb2f1945e218edf0e5a7312754c81
SSCharan-99 <saisricharan99@msitprogram.net>
2021-05-27 14:19:18 +0530

    ss

--- services.sh
+++ services.sh
@@ -1,3 +1,2 @@
-cd app/
-# Start rasa server with nlu model
+cd app
 rasa train && rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug \ -p $PORT
