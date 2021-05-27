25d7e558e0b50f65fa15bfaea01d0348b3d91d39
SSCharan-99 <72122904+SSCharan-99@users.noreply.github.com>
2021-05-27 14:50:14 +0530

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,2 +1,2 @@
-cd app
+cd /app
 rasa train && rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug \ -p $PORT
