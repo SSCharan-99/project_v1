17e05607dadd0d647baf7bfd2785a3b9aa8b6fcd
SSCharan-99 <saisricharan99@msitprogram.net>
2021-05-27 12:28:24 +0530

    changed

--- Dockerfile
+++ Dockerfile
@@ -24,5 +24,5 @@ ENV NODE_ENV=production
 RUN apt-get update && apt-get install -y python3 python3-pip && python3 -m pip install --no-cache --upgrade pip && pip3 install --no-cache rasa==2.6.0
 ADD . /app/
 RUN chmod +x /app/start_services.sh
-CMD ["/app/services.sh","/app/start_services.sh"]
+CMD ["/app/services.sh" "/app/start_services.sh"]
 
--- services.sh
+++ services.sh
@@ -1,4 +1,3 @@
-cd app/rasa/
+cd app/
 # Start rasa server with nlu model
-rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug \
-         -p $PORT
+rasa train && rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug \ -p $PORT
