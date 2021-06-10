32476b87bfc7de5e886618869c5fdbc3bb438b24
SSCharan-99 <saisricharan99@msitprogram.net>
2021-06-10 21:19:04 +0530

    Initialize the project

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
 cd app/
-
-rasa run --credentials ./credentials.yml  --enable-api --auth-token XYZ123 --model ./models --endpoints ./endpoints.yml --enable-api --cors "*" --debug 
+# Start rasa server with nlu model
+rasa run --model models --enable-api --cors "*" --debug \
+         -p $PORT
